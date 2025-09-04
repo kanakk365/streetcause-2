5db7b2396c65a1189448216d55d50aab3962aeb5
Kanak <kanakkumarmahala000@gmail.com>
2025-09-04 16:20:57 +0530

    fix : footer social links

--- src/app/Components/WhyFooterSection.tsx
+++ src/app/Components/WhyFooterSection.tsx
@@ -5,38 +5,15 @@ export const WhyFooterSection: React.FC = () => {
   const socialIcons = [
     {
       name: "X (Twitter)",
-      icon: `<svg width="44" height="43" viewBox="0 0 44 43" fill="none" xmlns="http://www.w3.org/2000/svg" style="width: 44px; height: 42px; border-radius: 75px; background: rgba(255, 255, 255, 0.48); padding: 31.844px 32.682px; cursor: pointer">
-        <rect y="0.787109" width="44" height="41.693" rx="20.8465" fill="white" fill-opacity="0.48"/>
-        <path d="M28.901 10.7871H32.581L24.541 19.9771L34 32.4801H26.594L20.794 24.8961L14.156 32.4801H10.474L19.074 22.6501L10 10.7881H17.594L22.837 17.7201L28.901 10.7871ZM27.61 30.2781L19.5 21.134L14.298 12.8741L27.61 30.2781Z" fill="white"/>
-      </svg>`
-    },
-    {
-      name: "Twitter",
-      icon: `<svg width="42" height="43" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg" style="width: 44px; height: 42px; border-radius: 75px; background: rgba(255, 255, 255, 0.48); padding: 31.844px 32.682px; cursor: pointer">
-        <rect y="0.787109" width="42" height="41.501" rx="20.7505" fill="white" fill-opacity="0.48"/>
-        <path d="M16.539 31.2882C25.596 31.2882 30.5505 23.7836 30.5505 17.2871C30.5505 17.0771 30.5505 16.8641 30.5415 16.6541C31.5061 15.9559 32.3386 15.0914 33 14.1011C32.099 14.4984 31.1441 14.7603 30.1665 14.8781C31.1963 14.2626 31.9676 13.2941 32.337 12.1526C31.3695 12.7258 30.3105 13.1282 29.2065 13.3421C28.4643 12.5517 27.4821 12.028 26.4121 11.8523C25.3422 11.6765 24.2441 11.8585 23.288 12.3701C22.3319 12.8816 21.5712 13.6941 21.1237 14.6817C20.6761 15.6694 20.5668 16.777 20.8125 17.8332C18.8546 17.735 16.9392 17.2264 15.1906 16.3403C13.4419 15.4542 11.899 14.2104 10.662 12.6896C10.034 13.7743 9.84236 15.0573 10.126 16.278C10.4096 17.4988 11.1472 18.5659 12.189 19.2626C11.4083 19.2361 10.6448 19.0265 9.96 18.6506V18.7181C9.96135 19.8544 10.355 20.9553 11.0743 21.8348C11.7937 22.7143 12.7946 23.3185 13.908 23.5452C13.4854 23.6616 13.0488 23.7196 12.6105 23.7177C12.3015 23.7186 11.9931 23.69 11.6895 23.6322C12.0042 24.6102 12.6169 25.4654 13.4419 26.0779C14.2668 26.6903 15.2627 27.0294 16.29 27.0477C14.5448 28.4184 12.3891 29.1618 10.17 29.1582C9.77898 29.1598 9.38823 29.1373 9 29.0907C11.2523 30.5265 13.8679 31.289 16.539 31.2882Z" fill="white"/>
-      </svg>`
+      icon: "/images/x.svg"
     },
     {
       name: "Facebook",
-      icon: `<svg width="43" height="42" viewBox="0 0 43 42" fill="none" xmlns="http://www.w3.org/2000/svg" style="width: 44px; height: 42px; border-radius: 75px; background: rgba(255, 255, 255, 0.48); padding: 31.844px 32.682px; cursor: pointer">
-        <rect y="0.787109" width="42.4" height="41" rx="20.5" fill="white" fill-opacity="0.48"/>
-        <path d="M18.398 30.7871H22.398V22.7771H26.002L26.398 18.7971H22.398V16.7871C22.398 16.5219 22.5034 16.2675 22.6909 16.08C22.8784 15.8925 23.1328 15.7871 23.398 15.7871H26.398V11.7871H23.398C22.0719 11.7871 20.8001 12.3139 19.8625 13.2516C18.9248 14.1893 18.398 15.461 18.398 16.7871V18.7971H16.398L16.002 22.7771H18.398V30.7871Z" fill="white"/>
-      </svg>`
+      icon: "/images/facebook.svg"
     },
     {
       name: "Instagram",
-      icon: `<svg width="43" height="42" viewBox="0 0 43 42" fill="none" xmlns="http://www.w3.org/2000/svg" style="width: 44px; height: 42px; border-radius: 75px; background: rgba(255, 255, 255, 0.48); padding: 31.844px 32.682px; cursor: pointer">
-        <g clip-path="url(#clip0_64_164)">
-          <rect x="0.399994" y="0.787109" width="42" height="41" rx="20.5" fill="white" fill-opacity="0.48"/>
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M16.6 9.28711C14.6904 9.28711 12.8591 10.0457 11.5088 11.3959C10.1586 12.7462 9.39999 14.5776 9.39999 16.4871L9.39999 26.0871C9.39999 27.9967 10.1586 29.828 11.5088 31.1783C12.8591 32.5285 14.6904 33.2871 16.6 33.2871H26.2C28.1096 33.2871 29.9409 32.5285 31.2912 31.1783C32.6414 29.828 33.4 27.9967 33.4 26.0871V16.4871C33.4 14.5776 32.6414 12.7462 31.2912 11.3959C29.9409 10.0457 28.1096 9.28711 26.2 9.28711H16.6ZM15.8 21.2871C15.8 19.8019 16.39 18.3775 17.4402 17.3273C18.4904 16.2771 19.9148 15.6871 21.4 15.6871C22.8852 15.6871 24.3096 16.2771 25.3598 17.3273C26.41 18.3775 27 19.8019 27 21.2871C27 22.7723 26.41 24.1967 25.3598 25.2469C24.3096 26.2971 22.8852 26.8871 21.4 26.8871C19.9148 26.8871 18.4904 26.2971 17.4402 25.2469C16.39 24.1967 15.8 22.7723 15.8 21.2871ZM27 15.6871H28.6V14.0871H27V15.6871Z" fill="white"/>
-        </g>
-        <defs>
-          <clipPath id="clip0_64_164">
-            <rect x="0.399994" y="0.787109" width="42" height="41" rx="20.5" fill="white"/>
-          </clipPath>
-        </defs>
-      </svg>`
+      icon: "/images/insta.svg"
     }
   ];
 
@@ -63,7 +40,7 @@ export const WhyFooterSection: React.FC = () => {
       </div>
 
       {/* Footer Content */}
-      <footer className="w-full shadow-[0_4px_16px_0_rgba(255,255,255,0.25)] pt-20 pb-12 px-0 max-sm:pt-[60px] max-sm:px-0">
+      <footer className="w-full  pt-20 pb-12 px-0 max-sm:pt-[60px] max-sm:px-0">
         <div className="flex gap-[50px] max-w-[1378px] mx-auto my-0 px-[62px] py-0 max-md:flex-col max-md:gap-10 max-md:px-10 max-md:py-0 max-sm:gap-[30px] max-sm:px-5 max-sm:py-0">
           <div className="flex flex-col items-start gap-6 w-[429px] max-md:w-full">
             <Image
@@ -85,10 +62,17 @@ export const WhyFooterSection: React.FC = () => {
                 {socialIcons.map((social, index) => (
                   <button
                     key={index}
-                    className="hover:opacity-80 transition-opacity"
+                    className="hover:opacity-80 transition-opacity w-13 h-13 rounded-full bg-[#b97c98] bg-opacity-20 flex items-center justify-center p-2"
                     aria-label={social.name}
-                    dangerouslySetInnerHTML={{ __html: social.icon }}
-                  />
+                  >
+                    <Image
+                      src={social.icon}
+                      alt={social.name}
+                      width={24}
+                      height={24}
+                      className="object-contain"
+                    />
+                  </button>
                 ))}
               </div>
             </div>
